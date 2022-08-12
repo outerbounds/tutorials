a94f9132d3e169d205f0bf53eebd5b2f19271c22
Hamel Husain <hamel.husain@gmail.com>
2022-08-12 11:17:00 -0700

    save

---
+++ nlp/baselineflow.py
@@ -0,0 +1,30 @@
+
+from metaflow import FlowSpec, step, Flow, current
+
+class BaselineNLPFlow(FlowSpec):
+        
+    @step
+    def start(self):
+        "Read the data"
+        import pandas as pd
+        self.df = pd.read_parquet('train.parquet')
+        self.valdf = pd.read_parquet('valid.parquet')
+        print(f'num of rows: {self.df.shape[0]}')
+        self.next(self.baseline)
+
+    @step
+    def baseline(self):
+        "Compute the baseline"
+        from sklearn.metrics import accuracy_score, roc_auc_score
+        baseline_predictions = [1] * self.valdf.shape[0]
+        self.base_acc = accuracy_score(self.valdf.labels, baseline_predictions)
+        self.base_rocauc = roc_auc_score(self.valdf.labels, baseline_predictions)
+        self.next(self.end)
+        
+    @step
+    def end(self):
+        print(f'Baseline Accuracy: {self.base_acc:.3f}\nBaseline AUC: {self.base_rocauc}')
+        
+
+if __name__ == '__main__':
+    BaselineNLPFlow()
